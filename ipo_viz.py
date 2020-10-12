4dada4fcee5847d923cc2bf372d6ac1c907626e4
docjsha <jing.sha@gmail.com>
2020-10-12 10:37:42 -0400

    Add files via upload

--- ipo_viz.py
+++ ipo_viz.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pandas as pd
 import requests
 import streamlit as st
@@ -46,47 +45,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-||||||| merged common ancestors
-=======
-import pandas as pd
-import requests
-import streamlit as st
-
-
-def main():
-    st.title('Stock Charts of Last 100 IPOs')
-
-    ipo_url = 'https://www.iposcoop.com/last-100-ipos/'
-    r = requests.get(ipo_url)
-    df_list = pd.read_html(r.text) 
-    df = df_list[0]
-    st.dataframe(df)
-    st.markdown('Data source: https://www.iposcoop.com/last-100-ipos')
-    st.markdown('---')
-
-    with st.spinner('Loading stock charts...'):
-        for t in reversed(df.Symbol):
-            url = f'https://finviz.com/chart.ashx?t={t}&ty=c&ta=1&p=d&s=l'
-            st.image(url, use_column_width=True)
-        
-    st.markdown('---')
-    st.markdown('Visualization: https://finviz.com/')
-
-    # Hide footer
-    hide_footer_style = """
-    <style>
-    .reportview-container .main footer {visibility: hidden;}
-    """
-    # Hide hamburger menu
-    st.markdown(hide_footer_style, unsafe_allow_html=True)
-    hide_menu_style = """
-        <style>
-        #MainMenu {visibility: hidden;}
-        </style>
-        """
-    st.markdown(hide_menu_style, unsafe_allow_html=True)
-
-
-if __name__ == "__main__":
-    main()
->>>>>>> 26f57f5bdf1fb3937f7cedcd5dc703d9c2a7f062
